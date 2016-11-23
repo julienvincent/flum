0cdb63d946e1e4f23b7fa083339136138c4a1425
Julien Vincent <julienlucvincent@gmail.com>
2016-11-23 16:00:30 +0200

    now exports flattenState separately

--- src/index.js
+++ src/index.js
@@ -5,4 +5,10 @@ export Form from './Form'
 export Input from './Input'
 export FormComponent from './FormComponent'
 
+import _ from 'lodash'
+export const flattenState = (state, search = false) => {
+    if (search && state.value !== undefined) return state.value
+    return _.mapValues(_.omitBy(state, val => typeof val === 'function'), val => flattenState(val, true))
+}
+
 export default createFactory(Form)
