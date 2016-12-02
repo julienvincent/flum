255ce9d0c7613380499e03015c97fe7ade525157
Julien Vincent <julienlucvincent@gmail.com>
2016-12-02 16:11:13 +0200

    export validators

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export Form from './Components/Form'
 export FormComponent from './Components/FormComponent'
 
+export validators from './Utils/Validators'
 export { validateField, validateState } from './Utils/Validation'
 export { flattenState } from './Utils/Tools'
